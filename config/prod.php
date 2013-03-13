ac03f00135599f46a2964fb36cfea7ab2bf47dd1
yohang <yohan@giarel.li>
2013-03-13 21:09:43 +0100

    Reverted some debug conf options

--- config/prod.php
+++ config/prod.php
@@ -3,4 +3,4 @@
 $this['satis.config_file']       = 'config.json';
 $this['gaufrette.adapter.class'] = 'Local';
 $this['gaufrette.options']       = array($this['app.data_dir']);
-$this['app.node_path']           = 'node';
+$this['app.node_path']           = '/usr/bin/node';
