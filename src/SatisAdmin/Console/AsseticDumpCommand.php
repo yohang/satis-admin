705bbe43e7bde5d1f05c354473ba9b255b49e981
yohang <yohan@giarel.li>
2013-07-11 20:18:56 +0200

    Updated travis script to use phantomjs instead of selenium

---
+++ src/SatisAdmin/Console/AsseticDumpCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SatisAdmin\Console;
+
+use Monolog\Logger;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Yohan Giarelli <yohan@frequence-web.fr>
+ */
+class AsseticDumpCommand extends BaseCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('assetic:dump')
+            ->setDescription('dump assets');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getApp()['assetic.dumper']->dumpAssets();
+    }
+}
