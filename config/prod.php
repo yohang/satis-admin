ea364ddac251374e62165a839e88500e05eebdff
yohang <yohan@giarel.li>
2016-08-31 16:53:30 +0200

    Updated npm / bower

--- config/prod.php
+++ config/prod.php
@@ -3,7 +3,7 @@
 $this['satis.config_file']       = 'config.json';
 $this['gaufrette.adapter.class'] = 'Local';
 $this['gaufrette.options']       = array($this['app.data_dir']);
-$this['app.node_path']           = '/usr/bin/node';
+$this['app.node_path']           = is_file('/usr/bin/node') ? '/usr/bin/node' : '/usr/local/bin/node';
 
 $this['satis_runner'] = $this->share(function() {
     return new SatisAdmin\Runner\SatisRunner(
